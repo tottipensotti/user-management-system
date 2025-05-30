29c64fc8ed9a48a9e54a00e21ebec67b4baa59bf
tottipensotti <ag.pensotti@gmail.com>
2025-05-29 21:59:11 -0300

    Logic refactoring: Created BaseUser and AdminUser classes with logic for each type of user.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from utils.utils import *
+from users import BaseUser, AdminUser, users
+
+admin_user = AdminUser(0, "Admin", "admin", "admin1234", "")
+regular_user = BaseUser(1, "Batman", "user", "TheDarkKnight123", "US")
+test_user = BaseUser(2, "John Doe", "user", "hello123", "US")
 
 if __name__ == '__main__':
-    # users.append(create_new_user(6, "Batman", "TheDarkKnight", "Gotham City"))
-    print(get_user_info(2))
-
-    update_user(2, {"user_name": "Michael Scott"})
-    update_user(9, {"user_role": "admin"})
+    regular_user.show_info()
+    admin_user.update_user(2, {"user_role": "test"})
+    admin_user.get_user_info(2)
--- users.py
+++ users.py
@@ -1,14 +1,61 @@
-class User:
-    def __init__(self, id, name, role, pwd, address):
+class BaseUser:
+    def __init__(self, id, name, role, pwd, country):
+        if any(user.user_id == id for user in users):
+            raise ValueError(f"User ID {id} already exists.")
+        
         self.user_id: int = id
         self.user_name: str = name
         self.user_role: str = role
         self._password: str = pwd
-        self.address: str = address
+        self.country: str = country
+        users.append(self)
+        print(f"User {self.user_id} successfully created.")
 
     def greet(self) -> None:
         print(f'Hello! My name is {self.user_name}')
     
-    def alter_role(self, new_role: str) -> None:
-        self.user_role = new_role
-        print(f"User role updated for user {self.user_id}: {self.user_role}")
+    def show_info(self) -> str:
+        user_info = (
+            "----------------------------------------\n"
+            "User information\n"
+            "----------------------------------------\n"
+            f"- User ID: {self.user_id}\n"
+            f"- Name: {self.user_name}\n"
+            f"- Country: {self.country}\n"
+        )
+        print(user_info)
+
+class AdminUser(BaseUser):
+    def __init__(self, id, name, role, pwd, country):
+        super().__init__(id, name, role, pwd, country)
+
+    def update_user(self, user_id: int, new_info: dict) -> None:
+        for i, user in enumerate(users):
+            if user.user_id == user_id:
+                for key in new_info:
+                    if hasattr(user, key):        
+                        setattr(users[i], key, new_info[key])
+                    else:
+                        e: str = f"{key} is not a valid attribute."
+                        raise ValueError(e)
+                print(f"Successfully updated user {user_id}.")
+                return
+
+        raise ValueError("User doesn't exist.")
+
+    
+    def delete_user(self, user_id: int) -> None:
+        for i, user in enumerate(users):
+            if user.user_id == user_id:
+                index: int = i
+        users.remove(users[index])
+        print(f"Succesfully deleted user {user_id}.")
+        
+        raise ValueError("User doesn't exists.")
+
+    def get_user_info(self, user_id: int) -> dict:
+        for user in users:
+            if user.user_id == user_id:
+                return user.__dict__
+    
+users: list[BaseUser] = []
